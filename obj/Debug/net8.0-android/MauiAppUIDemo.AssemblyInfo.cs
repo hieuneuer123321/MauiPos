65e15fc4310a9a9896e6caf33f5fee502746e495
VIT002(潘阮仲孝) <VIT002@sanfang.com>
2025-07-04 13:19:18 +0700

    end ui2

--- obj/Debug/net8.0-android/MauiAppUIDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/MauiAppUIDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiAppUIDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8d0d589d3c23ae07b9df7a27caad0ce1c087045f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a70eb8bc96cf042e93e78d980db00f6862ccd3ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiAppUIDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiAppUIDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
